adccfc7e6a0ddcddafa7916418e5a948ff4f722c
robertosn <robertosn@yahoo.es>
2021-01-13 11:11:29 +0100

    a

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Un monográfico realizado por <Link to="https://accuesp.com">ACCU España</Link>con el patrocinio de <Link to="https://www.takeda.com/es-es/">Takeda</Link></p>
+      <p>Un monográfico realizado por <Link to="https://accuesp.com">ACCU España</Link> con el patrocinio de <Link to="https://www.takeda.com/es-es/">Takeda</Link></p>
     </div>
   </footer>
 )
