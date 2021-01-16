3a332dd25882688a268f9dab579f2c8d0909d5f7
robertosn <robertosn@yahoo.es>
2021-01-16 09:57:39 +0100

    a

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,7 +3,9 @@ import { Link } from "gatsby"
 
 const Logo = (props) => (
   <div className="site-logo">
+    <a href="/">
    <img src="/assets/logo.jpg" alt="logo Salud y EII"/>
+   </a>
   </div>
 )
 
